7d057594518e7c23fabbb48e99b60c5b872b8d37
solloist <solloist@gmail.com>
2023-02-22 20:30:10 +0100

    Input summ

---
+++ main.js
@@ -0,0 +1,11 @@
+let second = document.querySelector("#second");
+second.addEventListener("keyup", foo);
+
+function foo(){
+    let a = document.querySelector("#first").value;
+    let b = document.querySelector("#second").value;
+    let p = document.createElement("p");
+    p.textContent = `${Number(a) + Number(b)}`;
+    let div = document.querySelector("#par");
+    div.appendChild(p);
+}
